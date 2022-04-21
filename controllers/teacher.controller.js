df5df86bf8e2e9f1e1116648cbad0a4f6f71356c
dali-trabelsi <mohamedmuhamed010@gmail.com>
2022-04-21 03:14:16 +0100

    added profile info get and update methods for teacher

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -1,5 +1,36 @@
+const courseModel = require("../models/course.model");
+const { USER_ROLES } = require("../models/enums");
 const Teacher = require("../models/teacher.model");
 
 exports.getProfileInfo = (req, res) => {
-  res.send({ message: "Teacher access!" });
+  Teacher.findById(res.locals.decodedJWT._id)
+    .lean()
+    .then((result) => {
+      res.status(200).json({ ...result, role: USER_ROLES.TEACHER });
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err.message });
+    });
+};
+
+exports.updateProfileInfo = (req, res) => {
+  if (req.file) {
+    req.body = { avatar: req.file.filename };
+  }
+  Teacher.findOneAndUpdate({ _id: res.locals.decodedJWT._id }, req.body)
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ ...result, role: USER_ROLES.TEACHER });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.message });
+    });
+};
+
+exports.getCourses = async (req, res) => {
+  const courses = await courseModel.find({
+    teacher: res.locals.decodedJWT._id,
+  });
+  res.json(courses);
 };
--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 const { authJwt } = require("../middlewares");
 const controller = require("../controllers/teacher.controller");
+const upload = require("../utils/multer");
 
-router.get("/info", [authJwt.verifyTeacherToken], controller.getProfileInfo);
+router.get(
+  "/user/info",
+  [authJwt.verifyTeacherToken],
+  controller.getProfileInfo
+);
+
+router.put(
+  "/user/info",
+  [authJwt.verifyTeacherToken],
+  upload.single("avatar"),
+  controller.updateProfileInfo
+);
+
+router.get("/my/courses", authJwt.verifyTeacherToken, controller.getCourses);
 
 module.exports = router;

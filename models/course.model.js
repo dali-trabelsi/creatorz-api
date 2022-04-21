49cb5f2f9d98e23b17a8becb967db3d7223945cb
dali-trabelsi <mohamedmuhamed010@gmail.com>
2022-04-21 03:11:38 +0100

    updated course model

--- models/course.model.js
+++ models/course.model.js
@@ -8,12 +8,12 @@ const courseSchema = new Schema(
   {
     title: { type: String, required: true },
     thumbnail: { type: String, required: true },
-    overview: { type: String, required: true },
+    overview: { type: String, required: false },
     description: { type: String, required: true },
     teacher: { type: ObjectId, required: true, ref: "Teacher" },
-    sections: [{ type: ObjectId, required: true, ref: "CourseSection" }],
+    sections: [{ type: ObjectId, ref: "CourseSection" }],
     isPublished: { type: Boolean, default: false },
-    publishedOn: { type: Boolean },
+    publishedOn: { type: Date },
   },
   { timestamps: true }
 );
